660fc90ec699fc7fce5875f120614e7020225373
Felipe Villarrubia <felipevillarrubiasymmes@gmail.com>
2024-02-06 00:38:52 -0300

    Se ajustan los archivos y carpetas a la ordenanza de Next.js. Se agrega al package.json el script para correr el servidor de desarrollo. Fin del capítulo 9

--- app/page.js
+++ app/page.js
@@ -4,7 +4,15 @@ function Header({ title }) {
     return <h1>{title ? title : 'Título por defecto'}</h1>
 }
 
-function HomePage() {
+// En Next, se usa un ruteo a través de sistemas de archivos. Para desplegar este index como la página de inicio
+// utilizaremos archivos y carpetas.
+// 1. Moveremos este archivo a una carpeta llamada App
+// 2. Renombraremos el archivo de index.js a page.js: de esta forma la declaramos como la página principal
+// 3. Agregaremos export default al componente HomePage para ayudar a Next.js a distinguirlo como el componente
+//    principal de la página.
+// 4. Para correr el servidor de desarrollo, indicaremos en package.json el script 'dev' y le asignaremos 'next dev'
+
+export default function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
 
     const [likes, setLikes] = useState(0)
